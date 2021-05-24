9eccdacda79b78843d32e9be4bce3692a78cd7f3
Angel Mejia <66014666+ins4w@users.noreply.github.com>
2021-05-24 11:05:47 -0500

    Source code

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
 
+// Min Heap: https://www.cs.usfca.edu/~galles/visualization/Heap.html
+
+int main() {
+  std::cout << "Hola!" << endl;
+  std::cout << "Puedes encontrar el ejemplo de heap en el siguiente enlace:\n https://www.cs.usfca.edu/~galles/visualization/Heap.html" << endl;
+  std::cout << "Se sugiere aplicar una reingeniería para poder hacer el max heap.";
+  return 0;
+}
