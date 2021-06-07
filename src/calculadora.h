0a68c04c6f9ec7c2e629c7d73cc5aef98a1a3dac
Jose Terrones <66368520+RadeonXYZ@users.noreply.github.com>
2021-06-06 22:49:47 -0500

    calculadora.h creado
    Avanze 1 Jose Terrones

---
+++ src/calculadora.h
@@ -0,0 +1,60 @@
+#ifndef __CALCULADORA_H__ 
+#define __CALCULADORA_H__ 
+
+#include <iostream> 
+#include <map>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+template <typename T>
+class Calculadora
+{
+    struct Node
+    {
+      T data;
+      Node *m_pChild[2]; //seleccion de nodo izquierdo y derecho
+      Node(const T &_data, Node *pLeft=nullptr, Node *pRight=nullptr) : data(_data)
+      {   m_pChild[0] = pLeft;  m_pChild[1] = pRight;   }
+      T getData()             { return data; }
+      void setData (T &_data) { data=_data; } 
+    };
+
+  class Numero
+  {
+    private:
+      T a; //primera numero
+      T b; //segunda numero
+      friend class Operacion; //usado en Operacion
+
+    public:
+  };
+
+  class Variable
+  {
+    private:
+      T x; //variable
+      friend class Operacion; //usado en Operacion
+      
+    public:
+
+  };
+
+  class Operacion
+  {
+    private:
+      T signo; //Signo en String
+
+    public:
+      /*
+      //futuro sustituto del switch
+      void ejec(const std::map<std::string, int>& m)
+      const auto& [signo, ] : m;
+
+      std::map<std::string,  > resultado { {"+", suma()}, {"-", resta()}, {"*", multiplicacion()}, {"/", division()} };
+      */
+  };
+};
+
+#endif
